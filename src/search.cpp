371b0380274d40635c37be16cd966e63ceb7b383
Kirill020708 <aboba>
2026-08-01 15:19:59 +0300

    Softer reduction for PV nodes ttData.value

--- src/search.cpp
+++ src/search.cpp
@@ -1299,7 +1299,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction for PvNodes (*Scaler)
         if (ss->ttPv)
-            r -= 3023 + PvNode * 1004 + (ttData.value > alpha) * 885
+            r -= 3023 + PvNode * 1004 + std::clamp(ttData.value - alpha, 0, 885)
                + (ttData.depth >= depth) * (816 + cutNode * 940);
 
         r += 697;  // Base reduction offset to compensate for other tweaks
